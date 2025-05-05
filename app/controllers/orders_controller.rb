43b772cff22aaa7642e3fa89a852654a879d1ece
csingh <chandambobo@gmail.com>
2025-05-05 06:21:06 +0000

    Update views in orders and villages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
     def index
       @orders_all = Order.all
-      @orders = @orders_all.page params[:page]
+      @orders = @orders_all.order(:date).page params[:page]
     end
   
     def show
